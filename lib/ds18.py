6b6558649dd9fd6571b0d3b62ce98815d14c52b1
ks-tec <sato@sag.ne.jp>
2020-12-14 09:39:43 +0900

    Added copyrights message.

--- lib/ds18.py
+++ lib/ds18.py
@@ -1,10 +1,22 @@
 # MicroPython DS18 thermistor driver, 1-wire interface.
 # Inherit the DS18X20 driver.
 #
-# added the return value returned by @property values.
-#   << return value >>
-#   * The modified return value have unit, it is tuple.
-#   * Temperature is up to the first decimal place.
+# Copyright (c) 2020 ks-tec
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to dealin the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sellcopies of the Software, and to permit persons to whom the Software
+# is furnished to do so, subject to the following conditions:
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE NOT LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS INTHE SOFTWARE.
 
 
 from micropython import const
--- lib/waterlevel.py
+++ lib/waterlevel.py
@@ -1,5 +1,22 @@
 # MicroPython Water Level Detector.
 # Using TouchPad pin on ESP32 board.
+#
+# Copyright (c) 2020 ks-tec
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to dealin the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sellcopies of the Software, and to permit persons to whom the Software
+# is furnished to do so, subject to the following conditions:
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE NOT LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS INTHE SOFTWARE.
 
 
 from micropython import const
